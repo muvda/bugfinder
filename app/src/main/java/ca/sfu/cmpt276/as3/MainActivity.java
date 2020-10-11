a58ae1c89f36759ac5db9a8ee478483c1fc0ac4d
Duy Anh Vu <duyanhv@sfu.ca>
2020-10-10 21:29:29 -0700

    Add main menu
    Add main menu activity, create game, help, option and welcome activities as placeholder

--- app/src/main/java/ca/sfu/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/sfu/cmpt276/as3/MainActivity.java
@@ -2,13 +2,27 @@ package ca.sfu.cmpt276.as3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Add animation here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButton();
+    }
+
+    private void setupButton() {
+        Button button = findViewById(R.id.button_main_skip);
+        button.setOnClickListener(v -> {
+            Intent intent = MenuActivity.makeIntent(MainActivity.this);
+            startActivity(intent);
+        });
     }
 }
